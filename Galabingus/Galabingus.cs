b1e78d60ae2ecdbf354e315ca9a603ab68112935
sgr2545 <sgr2545@rit.edu>
2023-03-09 13:33:18 -0500

    Created the UI Class

--- Galabingus/Galabingus.cs
+++ Galabingus/Galabingus.cs
@@ -9,6 +9,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Galabingus
 {
+
     public class Galabingus : Game
     {
         private GraphicsDeviceManager _graphics;
@@ -17,17 +18,19 @@ namespace Galabingus
         // GameObject Dynamic
         private dynamic content;
 
+        private UI userInterface;
+
         public Galabingus()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
@@ -35,25 +38,35 @@ namespace Galabingus
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            //new UI class and loading its content
+            userInterface = new UI(_graphics, Content, _spriteBatch);
+            userInterface.LoadContent();
+
             // Initalize the GameObject Instance and Content Dynamic
             content = GameObject.Instance.Initialize(Content, GraphicsDevice, _spriteBatch);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed 
+                || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            //update the game state
+            userInterface.Update();
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            //draw the screen
+            userInterface.Draw();
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Galabingus/UI.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace Galabingus
+{
+
+    public enum GameState
+    {
+        Menu,
+        Game
+    }
+
+    internal class UI
+    {
+        #region Fields
+
+        GameState gameState; //Gamestate variable
+
+        KeyboardState currentKBS;
+        KeyboardState prevKBS;
+
+        MouseState mouseState;
+
+        Texture2D playButtonTexture;
+
+        Rectangle playButtonRect;
+
+        int playButtonScale = 6;
+
+        int screenWidth;
+        int screenHeight;
+
+        GraphicsDeviceManager gr;
+        ContentManager cm;
+        SpriteBatch sb;
+
+        int[,] position;
+
+        #endregion
+
+        #region Constructor & Intialize
+
+        public UI(GraphicsDeviceManager gr, ContentManager cm, SpriteBatch sb)
+        {
+            this.gr = gr;
+            this.cm = cm;
+            this.sb = sb;
+
+            //Intializations
+            gameState = GameState.Menu;
+            currentKBS = Keyboard.GetState();
+            prevKBS = currentKBS;
+
+            playButtonScale = 6;
+
+            screenWidth = gr.PreferredBackBufferWidth;
+            screenHeight = gr.PreferredBackBufferHeight;
+        }
+
+        #endregion
+
+        #region LoadContent
+
+        public void LoadContent()
+        {
+            playButtonTexture = cm.Load<Texture2D>("PlayButton");
+            playButtonRect =
+                new Rectangle(
+                    (screenWidth - (playButtonTexture.Width / playButtonScale)) / 2,
+                    (screenHeight - (playButtonTexture.Height / playButtonScale)) / 2,
+                    (playButtonTexture.Width / playButtonScale),
+                    (playButtonTexture.Height / playButtonScale)
+                );
+
+            //TODO: File Loading
+
+
+            
+        }
+
+        #endregion
+
+        #region Update
+
+        public void Update()
+        {
+            currentKBS = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+
+            switch (gameState)
+            {
+                case GameState.Menu:
+
+                    if (mouseState.LeftButton == ButtonState.Pressed)
+                    {
+                        if (playButtonRect.Contains(mouseState.Position))
+                        {
+                            gameState = GameState.Game;
+                        }
+                    }
+
+                    if (SingleKeyPress(Keys.LeftShift)
+                        || SingleKeyPress(Keys.RightShift))
+                    {
+                        gameState = GameState.Game;
+                    }
+
+                    break;
+
+                case GameState.Game:
+
+                    if (SingleKeyPress(Keys.LeftShift)
+                        || SingleKeyPress(Keys.RightShift))
+                    {
+                        gameState = GameState.Menu;
+                    }
+
+                    break;
+
+            }
+
+            prevKBS = currentKBS;
+        }
+
+        #endregion
+
+        #region Draw
+
+        public void Draw()
+        {
+            switch (gameState)
+            {
+                case GameState.Menu:
+
+                   gr.GraphicsDevice.Clear(Color.DarkViolet);
+
+                    sb.Draw(
+                        playButtonTexture,
+                        playButtonRect,
+                        Color.White);
+
+                    break;
+
+                case GameState.Game:
+
+                    gr.GraphicsDevice.Clear(Color.DarkBlue);
+
+
+                    break;
+
+            }
+        }
+
+        #endregion
+
+        #region Helpers
+
+        public bool SingleKeyPress(Keys key)
+        {
+            if (currentKBS.IsKeyDown(key) && prevKBS.IsKeyUp(key))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
