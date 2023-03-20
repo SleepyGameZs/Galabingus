224405176d45f63f51b456ff6096836255c2857b
Jay Wong <jgw5189@rit.edu>
2023-03-19 22:35:25 -0400

    Added Borders

--- Galabingus/TileManager.cs
+++ Galabingus/TileManager.cs
@@ -66,8 +66,8 @@ namespace Galabingus
             ushort contentName = GameObject.Instance.Content.tile_strip26;
             Debug.WriteLine(contentName);
 
-            Tile tile = new Tile(contentName, 0, 0);
-            tileSize = new Vector2 (tile.Transform.Width, tile.Transform.Height);
+            //Tile tile = new Tile(contentName, 0, 0);
+            //tileSize = new Vector2 (tile.Transform.Width, tile.Transform.Height);
 
 
             layers = new List<ushort>();
@@ -114,14 +114,51 @@ namespace Galabingus
                     break;
             }
 
+            // Top
             currentSpriteNumber = (index);
             Tile tile = new Tile(GameObject.Instance.Content.white_pixel_strip1, 0, index, true);
             tile.Scale = 25f;
             tile.ScaleVector = new Vector2(100, 1);
             tile.Position = new Vector2(100, 100);
-
-
             tilesList.Add(tile);
+
+            // Bot
+            tile = new Tile(GameObject.Instance.Content.white_pixel_strip1, 1, index, true);
+            tile.Scale = 25f;
+            tile.ScaleVector = new Vector2(1000, 20);
+            tile.Position = new Vector2(0, screenSize.Y);
+            tilesList.Add(tile);
+
+            // Right
+            tile = new Tile(GameObject.Instance.Content.white_pixel_strip1, 2, index, true);
+            tile.Scale = 25f;
+            tile.ScaleVector = new Vector2(20, screenSize.Y);
+            tile.Position = new Vector2(screenSize.X+20, 0);
+            tilesList.Add(tile);
+
+            // Left
+            tile = new Tile(GameObject.Instance.Content.white_pixel_strip1, 3, index, true);
+            tile.Scale = 25f;
+            tile.ScaleVector = new Vector2(20, screenSize.Y);
+            tile.Position = new Vector2(-20, 0);
+            tilesList.Add(tile);
+        }
+
+        public void CreateBackground()
+        {
+            ushort instanceCounter = 0;
+            for (ushort i = 0; i < 100; i++)
+            {
+                for (ushort j = 0; j < 100; j++)
+                {
+                    Tile tile = new Tile(GameObject.Instance.Content.tile_strip26, instanceCounter, 0);
+                    tile.Scale = 3f;
+                    tile.ScaleVector = new Vector2(1, 1);
+                    tile.Position = new Vector2(tile.Transform.Width * j, tile.Transform.Height* i);
+                    tilesList.Add(tile);
+                    instanceCounter++;
+                }
+            }
         }
 
         public void Update(GameTime gameTime)
