864e7332aa499dc71dfd05c3ee8344c9c24365a2
MatthewGRodriguez <112350466+MatthewGRodriguez@users.noreply.github.com>
2023-03-30 21:36:49 -0400

    Shader Stuff

--- Galabingus/Galabingus.cs
+++ Galabingus/Galabingus.cs
@@ -99,10 +99,12 @@ namespace Galabingus
              * [3] -> Y Position
              */
             
+            
             l_a4_obj_enemyData.Add(new int[] { 1, 2, (int)(GameObject.Instance.GraphicsDevice.Viewport.Width * 2.4) - 67, 50 });
             l_a4_obj_enemyData.Add(new int[] { 1, 1, (int)(GameObject.Instance.GraphicsDevice.Viewport.Width * 2.4) - 67, GameObject.Instance.GraphicsDevice.Viewport.Height / 4 + 50 });
             l_a4_obj_enemyData.Add(new int[] { 1, 4, (int)(GameObject.Instance.GraphicsDevice.Viewport.Width * 2.4) - 67, (GameObject.Instance.GraphicsDevice.Viewport.Height / 4) * 2 + 50 });
             l_a4_obj_enemyData.Add(new int[] { 1, 5, (int)(GameObject.Instance.GraphicsDevice.Viewport.Width * 2.4) - 67, (GameObject.Instance.GraphicsDevice.Viewport.Height / 4) * 3 + 50 });
+            
 
             // Create a player
             player = new Player(new Vector2(GameObject.Instance.GraphicsDevice.Viewport.Height * 0.00875f, GameObject.Instance.GraphicsDevice.Viewport.Height * 0.00875f), content.player_strip5);
@@ -165,8 +167,14 @@ namespace Galabingus
 
             GraphicsDevice.Clear(Color.Transparent);
 
-            _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap, effect: shaders);
-
+            if (!(userInterface.GS == GameState.Menu))
+            {
+                _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap, effect: shaders);
+            }
+            else
+            {
+                _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap);
+            }
             //draw the screen
             userInterface.Draw();
 
