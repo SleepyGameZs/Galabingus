cac297689a9d480b3ae64df96448c2ba82decf5a
sgr2545 <sgr2545@rit.edu>
2023-04-21 13:32:43 -0400

    ok made some changes to the button witha new asset, I've also temporarily removed the other aspects of UI while I'm working on making the new assets

--- Galabingus/UIManager.cs
+++ Galabingus/UIManager.cs
@@ -307,11 +307,11 @@ namespace Galabingus
             event2 = HoverTexture;
             textEvent1 = null;
 
-            AddButton("buttonPlay_base_strip1", 0.6f,
+            button = AddButton("buttonPlay_base_strip1", 0.6f,
             new Vector2(width / 2, height / 2 + 30),
             event1, event2, menu1);
 
-            texture = cm.Load<Texture2D>("buttonPlay_hover");
+            texture = cm.Load<Texture2D>("buttonPlay_hover_strip1");
             button.HoverTexture = texture;
 
             //add the logo to the screen
