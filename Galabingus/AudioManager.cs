693fa8016fc446d2ec201ee956cfbcc2f5fb3580
Jay442 <55556269+Jay442@users.noreply.github.com>
2023-04-30 16:02:57 -0400

    Final cleanup of audio classes

--- Galabingus/AudioManager.cs
+++ Galabingus/AudioManager.cs
@@ -12,8 +12,32 @@ namespace Galabingus
 {
     public sealed class AudioManager
     {
+        #region Fields
+
+        // The instance of the audio manager
         private static AudioManager instance = null;
 
+        // List of all sounds
+        private List<Sound> sounds;
+
+        // List of all music
+        private List<Song> songCollection;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// List of all song objects in the game
+        /// </summary>
+        public List<Song> SongCollection
+        {
+            get { return songCollection; }
+        }
+
+        /// <summary>
+        /// Reference to the audio manager
+        /// </summary>
         public static AudioManager Instance
         {
             get
@@ -27,81 +51,130 @@ namespace Galabingus
 
         }
 
-        private List<Sound> sounds;
-        private List<Song> songCollection;
+        #endregion
 
-        public List<Song> SongCollection
-        {
-            get { return songCollection; }
-        }
+        #region Constructor
 
+        /// <summary>
+        /// Instantiates both the sound and music lists 
+        /// </summary>
         public AudioManager()
         {
             sounds = new List<Sound>();
             songCollection = new List<Song>();
         }
+        #endregion
 
+        #region Methods
 
+        /// <summary>
+        /// Adds a sound to the sounds list
+        /// </summary>
+        /// <param name="name"> Name used to identify the sound </param>
+        /// <param name="volume"> Base volume of the sound </param>
+        /// <param name="path"> File path for the imported file </param>
+        /// <param name="content"> Content manager used for importing </param>
         public void AddSound(string name, float volume, string path, ContentManager content)
         {
             sounds.Add(new Sound(name, volume, path, content));
         }
 
+        /// <summary>
+        /// Plays a sound effect from the sounds list
+        /// </summary>
+        /// <param name="name"> Name of the sound </param>
         public void CallSound(string name)
         {
+            // Search the sounds list for the sound
             for (int i = 0; i < sounds.Count; i++) 
             {
                 if (sounds[i].Name == name)
                 {
+                    // Create a new instance of the sound
                     sounds[i].CreateInstance();
+
+                    // Adjust the volume of the sound instance by master volume
                     sounds[i].AudioInstance.Volume = sounds[i].Volume * UIManager.Instance.MasterVolume;
-                    RunSound(sounds[i].AudioInstance.Volume, sounds[i].SoundEffect, sounds[i].AudioInstance);
+
+                    // Run the adjusted sound instance
+                    RunSound(sounds[i].AudioInstance.Volume, sounds[i].AudioInstance);
                 }
             }
         }
 
+        /// <summary>
+        /// Plays a sound effect from the sounds list
+        /// Does not allow for multiple instances to be played at once
+        /// </summary>
+        /// <param name="name"> Name of the sound </param>
         public void CallSoundOnce(string name)
         {
+            // Search the sounds list for the sound
             for (int i = 0; i < sounds.Count; i++)
             {
                 if (sounds[i].Name == name)
                 {
+                    // Adjust the volume of the sound instance by master volume
                     sounds[i].AudioInstance.Volume = sounds[i].Volume * UIManager.Instance.MasterVolume;
-                    RunSound(sounds[i].AudioInstance.Volume, sounds[i].SoundEffect, sounds[i].AudioInstance);
+
+                    // Run the adjusted sound instance
+                    RunSound(sounds[i].AudioInstance.Volume, sounds[i].AudioInstance);
                 }
             }
         }
 
+        /// <summary>
+        /// Plays a music track from the songs list
+        /// </summary>
+        /// <param name="name"> Name of the song </param>
         public void CallMusic(string name)
         {
+            // Reset media player volume
             MediaPlayer.Volume = 1f;
+
+            // Search the songs list for the music track 
             for(int i = 0;i < songCollection.Count; i++) 
             {
+                // Check to see if music is already being played or not
                 if (songCollection[i].Name == name && MediaPlayer.State != MediaState.Playing)
                 {
+                    // Adjust the media player's volume by master volume
                     MediaPlayer.Volume = MediaPlayer.Volume * UIManager.Instance.MasterVolume;
+
+                    // Play the adjusted song 
                     MediaPlayer.Play(songCollection[i]);
                 }
             }
         }
 
+        /// <summary>
+        /// Stops a sound from playing
+        /// </summary>
+        /// <param name="name"> Name of the sound </param>
         public void StopSound(string name)
         {
+            // Search the sounds list for the sound  
             for (int i = 0; i < sounds.Count; i++)
             {
                 if (sounds[i].Name == name)
                 {
+                    // Stop the playing of the instance
                     sounds[i].AudioInstance.Stop();
                 }
             }
         }
 
-        private void RunSound(float voulume, SoundEffect sound, SoundEffectInstance instance)
+        /// <summary>
+        /// Helper meathod for playing sounds
+        /// </summary>
+        /// <param name="voulume"> Volume of the instance </param>
+        /// <param name="instance"> The instance of the sound object </param>
+        private void RunSound(float voulume, SoundEffectInstance instance)
         {
             instance.Volume = voulume;
             instance.Play();
         }
 
-
+        #endregion
     }
 }
--- Galabingus/Sound.cs
+++ Galabingus/Sound.cs
@@ -10,6 +10,10 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
+// Sound Class
+/* Creates sound objects using imported sound files Monogame's SoundEffect Class
+ * Sounds have a volume that can changed and can be player ontop of one another */ 
+  
 namespace Galabingus
 {
     internal class Sound
@@ -19,45 +23,79 @@ namespace Galabingus
         // Name used to identify sound
         private string name;
 
+        // Volume of the sound
         private float volume;
+
+        // The sound that plays and its instance
         private SoundEffect sound;
         private SoundEffectInstance audioInstance;
+
         #endregion
 
         #region Properties
+
+        /// <summary>
+        /// Name of the sound object
+        /// </summary>
         public string Name
         {
             get { return name; } 
         }
 
+        /// <summary>
+        /// Volume of the base sound object
+        /// </summary>
         public float Volume
         {
             get { return volume; }
             set { volume = value; }
         }
 
+        /// <summary>
+        /// The sound that plays
+        /// </summary>
         public SoundEffect SoundEffect
         {
             get { return sound; }
         }
 
+        /// <summary>
+        /// The specfic instance of the sound object
+        /// </summary>
         public SoundEffectInstance AudioInstance
         {
             get { return audioInstance; } 
         }
+
         #endregion
 
         #region Constructor
+
+        /// <summary>
+        /// Creates a sound object with a base instance containing its name and volume 
+        /// </summary>
+        /// <param name="name"> Name of the sound </param>
+        /// <param name="volume"> Volume of the sound </param>
+        /// <param name="path"> File path for sound file </param>
+        /// <param name="content"> Content manager used for importing </param>
         public Sound(string name, float volume, string path, ContentManager content)
         {
             this.name = name;
             this.volume = volume;
+
+            // Importing of sound effect
             sound = content.Load<SoundEffect>(path);
+
+            // Creation of the base instance
             CreateInstance();
         }
         #endregion
 
         #region Methods
+
+        /// <summary>
+        /// Creates a new instance of a sound so that multiple instaces can run at once
+        /// </summary>
         public virtual void CreateInstance()
         {
             audioInstance = sound.CreateInstance();
